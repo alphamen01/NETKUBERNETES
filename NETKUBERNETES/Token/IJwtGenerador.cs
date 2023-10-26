710691371d6b7eb06fe80a6bb199430b12362b59
alphamen01 <lesg.2018@hotmail.com>
2023-10-25 22:26:05 -0500

    se creo la interfaz IJWTGENERADOR y la clase JWTGENERADOR con el metodo crear token

---
+++ NETKUBERNETES/Token/IJwtGenerador.cs
@@ -0,0 +1,9 @@
+﻿using NETKUBERNETES.Models;
+
+namespace NETKUBERNETES.Token
+{
+    public interface IJwtGenerador
+    {
+        string CrearToken(Usuario usuario);
+    }
+}
---
+++ NETKUBERNETES/Token/JwtGenerador.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
+using NETKUBERNETES.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace NETKUBERNETES.Token
+{
+    public class JwtGenerador : IJwtGenerador
+    {
+        public string CrearToken(Usuario usuario)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId,usuario.UserName!),
+                new Claim("userId", usuario.Id),
+                new Claim("email", usuario.Email!)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Mi palabra secreta"));
+            var credenciales  = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescripcion = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(30),
+                SigningCredentials = credenciales
+
+            };
+                 
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescripcion);
+
+            return tokenHandler.WriteToken(token);
+        } 
+    }
+        
+    
+}
